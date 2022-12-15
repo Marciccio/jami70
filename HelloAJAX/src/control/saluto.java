dee9f538f12550a96de44236d1bcb7d530891202
IlMaxone <maxo.paxo43@gmail.com>
2022-12-15 17:43:35 +0100

    ciao marci

--- HelloAJAX/src/control/saluto.java
+++ HelloAJAX/src/control/saluto.java
@@ -15,20 +15,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/saluto")
 public class saluto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public saluto() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		String user = request.getParameter("user");
 		PrintWriter out = response.getWriter();
 		if(user.equals("Giovanni")) {
@@ -37,13 +29,10 @@ public class saluto extends HttpServlet {
 		else {
 			out.print("Hello"+user);
 		}
+		System.out.println("Ciao Marci");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
